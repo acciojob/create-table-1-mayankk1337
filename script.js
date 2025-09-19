1c221a8050ed0968ffdc6d916e091418a5426958
mayankk1337 <ritiksingh2428@gmail.com>
2025-09-19 04:12:02 -0400

    created a submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function insert_Row() {
 	 var cell1=row.insertCell(0);
 	 var cell2=row.insertCell(1);
 	
-	 cell1.innerHTML = "NEW CELL1";
-     cell2.innerHTML = "NEW CELL2";
+	 cell1.innerHTML = "New Cell1";
+     cell2.innerHTML = "New Cell2";
   
 }
